0c60c299117bbb8c52e571924d6206a20e4161e1
Reese De Wind <Reese.DeWind@gmail.com>
2016-10-30 19:23:04 -0400

    added skeleton for scala player object, just to make sure everything compiles and imports correctly

---
+++ src/player001/RobotPlayer.scala
@@ -0,0 +1,18 @@
+import battlecode.common._;
+package player001{
+
+
+
+  //import java.util.Random;
+
+  class RobotPlayer {
+
+    /**
+      * run() is the method that is called when a robot is instantiated in the Battlecode world.
+      * If this method returns, the robot dies!
+      **/
+    def run(rc: RobotController) {
+
+    }
+  }
+}
